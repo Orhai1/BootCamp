b171c26d54404cbb3336a6040b210fec3acaef5f
Or Haibi <orhaibi1@gmail.com>
2025-08-11 19:54:57 +0300

    create the List (ex3)

--- subject4-react/src/Data Flow/components/List.jsx
+++ subject4-react/src/Data Flow/components/List.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 
-const List= () => {
+const List= ({contacts}) => {
   return (
     <div>
       <h2>Contacts</h2>
+      {contacts.map((c)=> (
+        <li key={c}> {c} </li>
+      ))}
     </div>
   );
 }
